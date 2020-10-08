200e9c400d0f6c8da234e935fae4ba9b1948c0c2
Caleb Chaney <calebc347@gmail.com>
2020-10-08 09:52:02 -0500

    Updated Javascript files

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,32 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+    constructor(name, id, email, gitHub) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.gitHub = gitHub;
+    }
+    getName() {
+
+    }
+
+    getId() {
+
+    }
+
+    getEmail() {
+
+    }
+
+    getGithub() {
+
+    }
+
+    getRole() {
+
+    } = engineer
+}
+
+module.exports = Engineer;
