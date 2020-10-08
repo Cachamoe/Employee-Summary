9ebb963977b588dbe729f56ce804723173c165af
Caleb Chaney <calebc347@gmail.com>
2020-10-08 16:27:58 -0500

    Updated Javascript

--- Develop/app.js
+++ Develop/app.js
@@ -10,57 +10,113 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
+promptUser();
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 function promptUser() {
-    return inquirer.prompt([
-        {
-            type: "list",
-            name: "employeetype",
-            question: "What is the employee type?",
-            choices: [
-                "Manager",
-                "Employee",
-                "Engineer",
-                "Intern",
-            ]
-        },
-        {
-            type: "input",
-            name: "name",
-            question: "What is the employee's name?"
-        },
-        {
-            type: "input",
-            name: "id",
-            question: "What is the employee's id?"
-        },
-        {
-            type: "input",
-            name: "email",
-            question: "What is the employee's email?"
-        },
-    ])
-        .then(answers => {
-            
-        })
-        .catch(error => {
-            if (error.isTtyError) {
-                // Prompt couldn't be rendered in the current environment
-            } else {
-                // Something else when wrong
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "employeeType",
+                message: "What is the employee type?",
+                choices: [
+                    Manager,
+                    Engineer,
+                    Intern,
+                ]
+            },
+        ]).then(answers => {
+            if (answers.choices === 0) {
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "name",
+                        question: "What is the manager's name?"
+                    },
+                    {
+                        type: "input",
+                        name: "id",
+                        question: "What is the manager's id?"
+                    },
+                    {
+                        type: "input",
+                        name: "email",
+                        question: "What is the manager's email?"
+                    },
+                    {
+                        type: "input",
+                        name: "officeNumber",
+                        question: "What is the manager's office number?"
+                    },
+                ])
+                const manager = new manager(answers.choices[0]).push(employees);
             }
-        }
-        )};
+            else if (answers.choices === 1) {
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "name",
+                        question: "What is the engineer's name?"
+                    },
+                    {
+                        type: "input",
+                        name: "id",
+                        question: "What is the engineer's id?"
+                    },
+                    {
+                        type: "input",
+                        name: "email",
+                        question: "What is the engineer's email?"
+                    },
+                    {
+                        type: "input",
+                        name: "github",
+                        question: "What is the engineer's github username?"
+                    },
+                ])
+                const engineer = new engineer(answers.choices[1]).push(employees);
+            }
+            else if (answers.choices === 2) {
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "name",
+                        question: "What is the intern's name?"
+                    },
+                    {
+                        type: "input",
+                        name: "id",
+                        question: "What is the intern's id?"
+                    },
+                    {
+                        type: "input",
+                        name: "email",
+                        question: "What is the intern's email?"
+                    },
+                    {
+                        type: "input",
+                        name: "school",
+                        question: "What is the intern's school?"
+                    },
+                ])
+                const intern = new intern(answers.choices[2]).push(employees);
+            }
+
+        }).catch(error => {
+            if (error) {
+                console.log("Error");
+            }
+        })      
+};
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
 
 render();
+let employees = [];
 
-const employeeObjects =
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
